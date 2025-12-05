83dbb93ca6ee9ddaa8bc9fdfc8cd74e842e0579d
NullVerdict <24498484+NullVerdict@users.noreply.github.com>
2025-12-05 01:56:55 +0000

    chore: upgrade dependencies and apply dart fixes [skip ci]

--- lib/screens/about_page.dart
+++ lib/screens/about_page.dart
@@ -1,5 +1,4 @@
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:material_symbols_icons/material_symbols_icons.dart';
