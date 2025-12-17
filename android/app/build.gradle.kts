7c8f6abe60583987aefb58926ed2877921f7b799
NullVerdict <none@none.none>
2025-12-18 03:42:19 +0400

    test

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 android {
     namespace = "com.pranshulgg.weather_master_app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 34
     ndkVersion = "27.0.12077973"
     
 
@@ -33,7 +33,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
